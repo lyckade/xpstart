8d912d3c408c03b71e7209549cdd9e6b48f3cc08
Andreas Schroepfer <as@lycka.de>
2014-08-07 15:14:03 +0200

    New ExclusionScenery.zip and class corrections

--- xpstart/exclusion.py
+++ xpstart/exclusion.py
@@ -1,7 +1,15 @@
 class Exclusion():
-    def __init__(self):
+    def __init__(self, xp_path):
+
+        if xp_path.endswith("/") or xp_path.endswith("\\"):
+            xp_path = xp_path[:-1]
+        self.xp_path = xp_path
+
+        self.scenery_path = "%s/Custom Scenery" % (self.xp_path)
+
         self.exclusions_archive_file = "ExclusionScenery.zip"
         self.exclusions_archive = False
+
 
     def close_archive(self):
         if self.exclusions_archive:
@@ -11,14 +19,14 @@ class Exclusion():
         if not self.exclusions_archive:
             return False
         icao_exists = False
-        path = "ExclusionScenery/Exclusion_%s/" % (icao)
+        path = "Exclusion_%s/" % (icao)
 
         nl = self.exclusions_archive.namelist()
         for f in nl:
             if f.startswith(path):
                 icao_exists = True
                 print f
-                self.exclusions_archive.extract(f)
+                self.exclusions_archive.extract(f, self.scenery_path)
         return icao_exists
 
     def open_archive(self, filename=""):
@@ -29,7 +37,3 @@ class Exclusion():
         self.exclusions_archive = zipfile.ZipFile(filename, "r")
 
 
-# test = Exclusion()
-# test.open_archive()
-# test.extract_scenery("EDDH")
-# test.close_archive()
